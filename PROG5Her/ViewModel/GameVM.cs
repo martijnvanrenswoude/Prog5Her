cf2d7b408695f8e572dea0f7faef882be359f7c9
martijnvanrenswoude <mrenswou@avans.nl>
2018-12-14 15:01:29 +0100

    GameVM Base

---
+++ PROG5Her/ViewModel/GameVM.cs
@@ -0,0 +1,31 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PROG5Her.ViewModel
+{
+    class GameVM : ViewModelBase
+
+    {
+        //vars
+        //properties
+        public List<Questionnaire> AllQuestionnaires { get; set; }
+        public Questionnaire SelectedQuestionnaire { get; set; }
+
+        //constructor
+        public GameVM()
+        {
+
+        }
+        //methods
+        public void GetAllQuestionnairesFromDatabase()
+        {
+            using(var context = new QuizDBEntities())
+            {
+                AllQuestionnaires = context.Questionnaires.ToList();
+            }
+        }
+    }
+}
