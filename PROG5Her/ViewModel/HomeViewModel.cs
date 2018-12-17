7cbde571622e5c96a462e4f8ff76cd71f0878443
WesselvonderFuhr <wesselvonderfuhr@gmail.com>
2018-12-17 20:20:37 +0100

    trein werk

--- PROG5Her/ViewModel/HomeViewModel.cs
+++ PROG5Her/ViewModel/HomeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using GalaSoft.MvvmLight.Command;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using PROG5Her.View;
 using System;
 using System.Collections.Generic;
@@ -8,12 +9,13 @@ using System.Text;
 
 namespace PROG5Her.ViewModel
 {
-    public class HomeViewModel : ViewBase
+    public class HomeViewModel : ViewModelBase
     {
         //vars
         private AddQuestionAndAnswerView questionCrudView;
         private QuizCRUDView quizCrudView;
         //properties
+        public Questionnaire SelectedQuiz { get; set; }
         public AddQuestionAndAnswerView QuestionCrudView
         {
             get {return questionCrudView;}
@@ -24,19 +26,29 @@ namespace PROG5Her.ViewModel
             get { return quizCrudView; }
             set { quizCrudView = value; }
         }
-        public ObservableCollection test;
+        public List<Questionnaire> AllQuizes { get; set; }
         //commands
+        public RelayCommand PlayQuizCommand { get; set; }
         public RelayCommand OpenQuestionCrudViewCommand { get; set; }
         public RelayCommand OpenQuizCrudViewCommand { get; set; }
         //constructor
         public HomeViewModel()
         {
+            //lists
+            getAllQuizes();
             //set commands
+            PlayQuizCommand =               new RelayCommand(PlayQuiz);
             OpenQuestionCrudViewCommand =   new RelayCommand(OpenQuestionCrudView);
             OpenQuizCrudViewCommand =       new RelayCommand(OpenQuizCrudView);
         }
         //functions
-
+        private void getAllQuizes()
+        {
+            using (var context = new QuizDBEntities())
+            {
+                AllQuizes = context.Questionnaires.ToList();
+            }
+        }
         //command functions
         private void OpenQuestionCrudView()
         {
@@ -47,6 +59,11 @@ namespace PROG5Her.ViewModel
         {
             QuizCrudView = new QuizCRUDView();
             QuizCrudView.Show();
+        }
+
+        private void PlayQuiz()
+        {
+            throw new NotImplementedException();
         }
     }
 }
